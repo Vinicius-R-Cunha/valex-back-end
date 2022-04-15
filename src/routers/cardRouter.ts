4b0204cab1a447e9a98004c0326e68d5815940c3
Vinicius <vinicunha2008@gmail.com>
2022-04-15 07:35:42 -0300

    project init

---
+++ src/routers/cardRouter.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { createCard } from "../controllers/cardController.js";
+
+const cardRouter = Router();
+cardRouter.post('/cards', createCard);
+
+export default cardRouter;
